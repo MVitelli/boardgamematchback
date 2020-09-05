58962d84368cc546fc30629434ce037adea63dff
Mvitelli <movitelli@gmail.com>
2020-09-05 00:08:12 -0300

    subo el controller con una consulta básica por id

---
+++ Controllers/game.js
@@ -0,0 +1,12 @@
+const GameModel = require('../models/game');
+
+exports.getById = (req, res) => {
+    GameModel.findById(req.params.gameId)
+        .then((result) => {
+            if (!result) return res.status(404).send({ message: 'EL usuario no existe' });
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err })
+        })
+};
